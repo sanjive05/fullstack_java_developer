1817cedd6200903d5065a1064f9c3155b88aca22
sanjive05 <sanjivesubramaniam4154@gmail.com>
2025-04-27 23:35:40 +0530

    JPA

--- spring_boot/jpa_hands_on/media/src/main/java/com/social/media/models/Social_Profile.java
+++ spring_boot/jpa_hands_on/media/src/main/java/com/social/media/models/Social_Profile.java
@@ -1,4 +1,15 @@
 package com.social.media.models;
 
+import jakarta.persistence.*;
+
+
+@Entity
 public class Social_Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "socialProfile")
+    private Social_User socialUser;
 }
--- spring_boot/jpa_hands_on/media/src/main/java/com/social/media/models/Social_User.java
+++ spring_boot/jpa_hands_on/media/src/main/java/com/social/media/models/Social_User.java
@@ -1,5 +1,17 @@
 package com.social.media.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Social_User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "social_profile_id")
+    private Social_Profile socialProfile;
+
+
+
 }
