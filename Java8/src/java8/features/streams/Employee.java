10907904239573bf68f9aacb3e12046fe9433a70
sanjive05 <sanjivesubramaniam4154@gmail.com>
2025-05-10 22:24:03 +0530

    java 8 streams

--- Java8/src/java8/features/streams/Employee.java
+++ Java8/src/java8/features/streams/Employee.java
@@ -1,5 +1,40 @@
 package java8.features.streams;
 
 public class Employee {
+	
+	private String name;
+	private int age;
+	private String department;
+	
+	
+	public Employee(String name, int age, String department) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.department = department;
+	}
+	public Employee() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
 
 }
--- Java8/src/java8/features/streams/EmployeeStreamQuestion.java
+++ Java8/src/java8/features/streams/EmployeeStreamQuestion.java
@@ -1,9 +1,18 @@
 package java8.features.streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EmployeeStreamQuestion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Employee> list = Arrays.asList(new Employee("Sanjive",23,"CCD"),new Employee("Robert",30,"Accounts"),
+				new Employee("Pradeep",24,"Accounts"),new Employee("Hariharan",27,"ADM"));
+		
+		list.stream().filter(e->e.getAge()>25).forEach(e->System.out.println(e.getAge()));
+		
+		list.stream().map(e -> e.getName().toUpperCase()).forEach(e->System.out.println(e));
 
 	}
 
