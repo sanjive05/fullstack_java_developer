fbead44ad411c8014f7f23e41ef98d0f758a4e96
sanjive05 <sanjivesubramaniam4154@gmail.com>
2025-05-23 00:04:05 +0530

    Multithreading in singleton

--- DSA/Interview/src/multithreading/Stack.java
+++ DSA/Interview/src/multithreading/Stack.java
@@ -3,9 +3,12 @@ package multithreading;
 public class Stack {
 	private int[] array;
 	private int stackTop;
+	
+	private Object lock;
 	public Stack(int capacity) {
 		array = new int[capacity];
 		stackTop = -1;
+		new Object();
 	}
 	
 	public boolean isEmpty() {
@@ -15,33 +18,40 @@ public class Stack {
 	public boolean isFull() {
 		return stackTop >= array.length-1;
 	}
-	public synchronized boolean push(int data) {
-		if(isFull()) {
-			return false;
+	public  boolean push(int data) {
+		
+		synchronized (lock) {
+			if(isFull()) {
+				return false;
+			}
+			++stackTop;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(Exception e) {
+				
+			}
+			array[stackTop] = data;
+			return true;
 		}
-		++stackTop;
-		try {
-			Thread.sleep(1000);
-		}
-		catch(Exception e) {
-			
-		}
-		array[stackTop] = data;
-		return true;
+		
 	}
-	public synchronized int pop() {
-		if(isEmpty()) {
-			return Integer.MIN_VALUE;
+	public  int pop() {
+		synchronized(lock) {
+			if(isEmpty()) {
+				return Integer.MIN_VALUE;
+			}
+			int obj = array[stackTop];
+			array[stackTop] = Integer.MIN_VALUE;
+			try {
+				Thread.sleep(1000);
+			}
+			catch(Exception e) {
+				
+			}
+			stackTop--;
+			return obj;
 		}
-		int obj = array[stackTop];
-		array[stackTop] = Integer.MIN_VALUE;
-		try {
-			Thread.sleep(1000);
-		}
-		catch(Exception e) {
-			
-		}
-		stackTop--;
-		return obj;
+		
 	}
 }
