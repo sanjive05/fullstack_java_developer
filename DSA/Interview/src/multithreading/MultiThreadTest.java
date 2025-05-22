f8b9fdc28c114948925a83cc51bd852f18364ff7
sanjive05 <sanjivesubramaniam4154@gmail.com>
2025-05-22 22:06:18 +0530

    Multithreading in singleton

--- DSA/Interview/src/multithreading/MultiThreadTest.java
+++ DSA/Interview/src/multithreading/MultiThreadTest.java
@@ -5,7 +5,7 @@ public class MultiThreadTest {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Main is starting...");
 		
-		Thread thread2 = new Thread(new Thread2(),"thread 2");
+		//Thread thread2 = new Thread(new Thread2(),"thread 2");
 		Thread thread3 = new Thread(()->{
 			for(int i=0;i<1000;i++) {
 				System.out.println(" "+i+"  "+Thread.currentThread());
@@ -13,9 +13,9 @@ public class MultiThreadTest {
 		}," Thread 3");
 		thread3.setDaemon(true);
 
-		thread3.start();
+		//thread3.start();
 
-		thread2.start();
+		//thread2.start();
 		
 		
 		/*
@@ -26,8 +26,26 @@ public class MultiThreadTest {
 		System.out.println(thread1.getName());
 		
 		*/
+		Thread thread1 = new Thread(()->{
+			SingletonClassMultithreading obj = SingletonClassMultithreading.getObject();
+			obj.print();
+			System.out.println(Thread.currentThread()  +"   "+ System.identityHashCode(obj));
+		},"Thread 1");
+		
+		
+		Thread thread2 = new Thread(()->{
+			SingletonClassMultithreading obj = SingletonClassMultithreading.getObject();
+			obj.print();
+			System.out.println(Thread.currentThread()  +"   "+ System.identityHashCode(obj));
+		},"Thread 2");
+		
+		
+		thread1.start();
+		thread2.start();
+		
 		
 		System.out.println("Main is ending...");
+		
 	}
 	
 
---
+++ DSA/Interview/src/multithreading/SingletonClassMultithreading.java
@@ -0,0 +1,21 @@
+package multithreading;
+
+public class SingletonClassMultithreading {
+	private static SingletonClassMultithreading singleTonClassMultithreading;
+	
+	private SingletonClassMultithreading() {
+		System.out.println("Object created");
+	}
+	
+	public static SingletonClassMultithreading getObject() {
+		if(singleTonClassMultithreading == null) {
+		      singleTonClassMultithreading = new SingletonClassMultithreading();
+		}
+		return singleTonClassMultithreading;
+	}
+	
+	public void print() {
+		System.out.println("This is Singleton Class...");
+	}
+
+}
