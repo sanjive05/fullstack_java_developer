52ac02d1714c3734eb675ff6f55af5bf56b73f32
sanjive05 <sanjivesubramaniam4154@gmail.com>
2025-05-27 22:20:15 +0530

    ecom

---
+++ DSA/DatastructuresAndAlgorithms/src/arrays/FindTheLastDigitOfBiggestNumber.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class FindTheLastDigitOfBiggestNumber {
+
+	public static void main(String[] args) {
+		String input = "3434";
+		int a = input.charAt(input.length()-1)-'0';
+		System.out.println(a);
+
+	}
+
+}
