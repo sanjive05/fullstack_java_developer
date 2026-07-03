cc9715dfa00e7e14965e593f85638967ad844228
sanjive05 <sanjivesubramaniam4154@gmail.com>
2026-07-03 22:46:17 +0530

    java basics

--- java/src/main/java/leetcode/ThreeSum.java
+++ java/src/main/java/leetcode/ThreeSum.java
@@ -7,21 +7,33 @@ public class ThreeSum {
     public static void main(String[] args) {
         List<List<Integer>> subArr = new ArrayList<>();
         int[] arr = {-1,0,1,2,-1,-4};
-        int i=0 , j=1;
 
-        for(i=0;i<arr.length;i++){
-            List<Integer> temp = new ArrayList<>();
-            temp.add(arr[i]);
-            for(j=i+1;j<arr.length;j++){
-                temp.add(arr[j]);
-                if(temp.size()==3){
-                    subArr.add(temp);
-                    temp= new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(arr[i]);
+                    list.add(arr[j]);
+                    list.add(arr[k]);
+                    subArr.add(list);
                 }
             }
         }
         System.out.println(subArr);
 
+        for(List<Integer> temp : subArr){
+            if(findSum((ArrayList<Integer>) temp)){
+                System.out.println(temp);
+            }
+        }
 
+
+    }
+
+    static boolean findSum(ArrayList<Integer> list){
+        if((list.get(0)+list.get(1)+list.get(2)) ==0 ){
+            return true;
+        }
+        return false;
     }
 }
