40d91ca9ef427680052bcfb6bca0e9485162e360
sanjive05 <sanjivesubramaniam4154@gmail.com>
2026-07-04 22:04:10 +0530

    java basics

---
+++ java/src/main/java/org/core/CheckLastDigitFour.java
@@ -0,0 +1,9 @@
+package org.core;
+
+public class CheckLastDigitFour {
+    public static void main(String[] args) {
+        int n = 342;
+        int lastDigit = 342%10;
+        System.out.println(lastDigit);
+    }
+}
